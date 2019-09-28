cbfb2272397dbcfedd528774fb24f8cf61a43d9c
elijahmm17 <elijahmm17@gmail.com>
2019-09-28 00:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 }
 
 function exactMatch(drivers, match) {
-  const matches = false;
+  let matches = false;
   return drivers.filter(function(driver) {
     for (const key in match) {
       matches = driver[key] === match;

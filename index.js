538df3703f78032f710a13229192f9860d17396a
elijahmm17 <elijahmm17@gmail.com>
2019-09-28 00:55:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function exactMatch(drivers, match) {
   const matches = false;
   return drivers.filter(function(driver) {
     for (const key in match) {
-      matches =  driver[key] === match;
+      matches = driver[key] === match;
     }
   });
 }
